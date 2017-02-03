b2a828405aef72ebd94ed2a34f3a29be5333625c
zhangzju <zhangway19921221@163.com>
2017-02-03 14:28:26 +0800

    first time run

--- .next/dist/pages/_error.js
+++ .next/dist/pages/_error.js
@@ -4,31 +4,31 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _getPrototypeOf = require('D:\\workspace\\learnEcharts\\node_modules\\babel-runtime/core-js/object/get-prototype-of');
+var _getPrototypeOf = require('C:\\Users\\Administrator\\Desktop\\learnEcharts\\node_modules\\babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
 
-var _classCallCheck2 = require('D:\\workspace\\learnEcharts\\node_modules\\babel-runtime/helpers/classCallCheck');
+var _classCallCheck2 = require('C:\\Users\\Administrator\\Desktop\\learnEcharts\\node_modules\\babel-runtime/helpers/classCallCheck');
 
 var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
-var _createClass2 = require('D:\\workspace\\learnEcharts\\node_modules\\babel-runtime/helpers/createClass');
+var _createClass2 = require('C:\\Users\\Administrator\\Desktop\\learnEcharts\\node_modules\\babel-runtime/helpers/createClass');
 
 var _createClass3 = _interopRequireDefault(_createClass2);
 
-var _possibleConstructorReturn2 = require('D:\\workspace\\learnEcharts\\node_modules\\babel-runtime/helpers/possibleConstructorReturn');
+var _possibleConstructorReturn2 = require('C:\\Users\\Administrator\\Desktop\\learnEcharts\\node_modules\\babel-runtime/helpers/possibleConstructorReturn');
 
 var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
 
-var _inherits2 = require('D:\\workspace\\learnEcharts\\node_modules\\babel-runtime/helpers/inherits');
+var _inherits2 = require('C:\\Users\\Administrator\\Desktop\\learnEcharts\\node_modules\\babel-runtime/helpers/inherits');
 
 var _inherits3 = _interopRequireDefault(_inherits2);
 
-var _react = require('D:\\workspace\\learnEcharts\\node_modules\\react\\react.js');
+var _react = require('C:\\Users\\Administrator\\Desktop\\learnEcharts\\node_modules\\react\\react.js');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _css = require('D:\\workspace\\learnEcharts\\node_modules\\next\\dist\\lib\\css.js');
+var _css = require('C:\\Users\\Administrator\\Desktop\\learnEcharts\\node_modules\\next\\dist\\lib\\css.js');
 
 var _css2 = _interopRequireDefault(_css);
 
